2508f24f8bcf344353b1ad36aa02f3fe88e0a7ea
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-01-30 17:22:18 +0300

    Summary

--- openpyxl_test/test.py
+++ openpyxl_test/test.py
@@ -3,18 +3,37 @@ import openpyxl
 # Читаем excel-файл:
 wb = openpyxl.load_workbook('reestr_podryadchiki.xlsx')
 
+sheets = wb.sheetnames
+
+# ====== Получаем нужный нам лист по индексу: ====== #
+sheet = wb[sheets[0]]
+
 # ====== Печатаем список листов: ====== #
-# sheets = wb.sheetnames
 # for sheet in sheets:
     # print(sheet)
     
 # ====== Получаем активный лист: ====== #
-sheet = wb.active
-print(sheet)
+# sheet = wb.active
+# print(sheet)
+
+
+# ====== Печатаем нужный нам лист: ====== #
+# print(sheet)
 
 # ====== Печатаем тип и значение содержимого ячейки: ====== #
-print(type(sheet['J5'].value))
-print(sheet['J5'].value)
+# print(type(sheet['J28'].value))
+# print(sheet['J28'].value)
+
+# ====== Проходим циклом по типам и значениям содержимых нужной нам колонки (колонки - латинские буквы, строки - числа): ====== #
+# n = 1
+# while n <= 36:
+    # print(type(sheet[f'H{n}'].value))
+    # print(sheet[f'H{n}'].value)
+    # n += 1
+column_h = sheet['H']
+for cell in column_h:
+    print(type(cell.value))
+    print(cell.value)
 
 # ====== Выводим значения всех ячеек активного листа: ====== #
 # for row in sheet.rows:
