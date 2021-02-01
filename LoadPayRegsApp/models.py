a60830c0bc5011c567d1a090d64f64e85de22f66
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-02-01 22:56:56 +0300

    Summary

--- LoadPayRegsApp/models.py
+++ LoadPayRegsApp/models.py
@@ -1,4 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here:
+
+class Document(models.Model):
+    docfile = models.FileField(upload_to='docs/')
+
 class TableColumns(models.Model):
+    organization
+    material_assigning
+    account_purpose
+    contract_number
+    contract_amount
+    paid1
+    account_number
+    account_date
+    accounts_amount
+    for_payment
+    payment_balance
+    paid2
+    remainder
+    note
+    result_sum1
+    result_sum2
--- LoadPayRegsApp/views.py
+++ LoadPayRegsApp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+
+# Воображаемая функция для обработки загруженного файла:
+### ...
+
+# Create your views here:
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
