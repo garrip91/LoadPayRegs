9f706e4b3ac3eab6912f29c586f1b4f2bec8fe1e
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-02-08 21:30:33 +0300

    Summary

--- LoadPayRegsApp/models.py
+++ LoadPayRegsApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here:
 class TableAndUrlColumns(models.Model):
+    docfile = models.FileField(upload_to='docs/', verbose_name="Загрузка файла", null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
     organization = models.CharField(max_length=255, verbose_name="Организация", null=True, blank=True)
     material_assigning = models.CharField(max_length=255, verbose_name="Назначение материала", null=True, blank=True)
     account_purpose = models.CharField(max_length=255, verbose_name="Назначение счёта", null=True, blank=True)
@@ -16,13 +18,20 @@ class TableAndUrlColumns(models.Model):
     paid2 = models.DecimalField(max_digits=14, decimal_places=2, verbose_name="Оплачено", null=True, blank=True)
     remainder = models.DecimalField(max_digits=14, decimal_places=2, verbose_name="Остаток", null=True, blank=True)
     note = models.CharField(max_length=255, verbose_name="Примечание", null=True, blank=True)
-    docfile = models.FileField(upload_to='docs/', verbose_name="Загрузка файла", null=True, blank=True)
+    total = models.DecimalField(max_digits=14, decimal_places=2, verbose_name="Остаток", null=True, blank=True)
     #result_sum1 = TableColumns.objects.aggregate(Sum('accounts_amount'))
     #result_sum2 = TableColumns.objects.aggregate(Sum('payment_balance'))
     
     class Meta:
         verbose_name = "Запись с реестра"
         verbose_name_plural = "Записи с реестра"
+        ordering = ['organization']
         
     def __str__(self):
         return self.organization
+        
+    # ====== Пример для корректировки: ======
+    #def save(self, *args, **kwargs):
+        #if not self.slug:
+            #self.slug = from_cyrillic_to_eng(str(self.name))
+        #super().save(*args, **kwargs)
--- LoadPayRegsApp/utils.py
+++ LoadPayRegsApp/utils.py
@@ -1,7 +1,9 @@
 from django.db.models import Sum
 
 @property
-def result_sum(self):
-    result_sum1 = TableAndUrlColumns.objects.filter(accounts_amount__isnull=True).aggregate(Sum('accounts_amount'))
-    result_sum2 = TableAndUrlColumns.objects.filter(payment_balance__isnull=True).aggregate(Sum('payment_balance'))
+def total_result(self):
+    total_result1 = TableAndUrlColumns.objects.filter(accounts_amount__isnull=True).aggregate(Sum('accounts_amount'))
+    total_result2 = TableAndUrlColumns.objects.filter(payment_balance__isnull=True).aggregate(Sum('payment_balance'))
     return (result_sum1, result_sum2)
+    
+print(total_result)
